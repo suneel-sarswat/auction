4cc9be727186c19cfd751b31b16dd5c4ffbe6460
Abhishek Kr Singh <abhishek.uor@gmail.com>
2018-12-24 16:10:29 +0530

    Add files via upload

---
+++ GenReflect.v
@@ -0,0 +1,99 @@
+
+
+
+(* -----------------Description------------------------------------------
+
+This file contains useful results about reflection techniques in Coq. 
+Some of the important concepts formalized are:
+
+Definition Prop_bool_eq (P: Prop) (b: bool):= P <-> b=true.
+Lemma reflect_intro (P:Prop)(b:bool): Prop_bool_eq P b -> reflect P b.
+
+Lemma impP(p q:bool): reflect (p->q)((negb p) || q).
+Lemma switch1(b:bool): b=false -> ~ b. 
+Lemma switch2(b:bool): ~ b -> b=false.
+
+
+Some useful Ltac defined terms:
+
+ Ltac switch:=  (apply switch1||apply switch2).
+ Ltac switch_in H:= (apply switch1 in H || apply switch2 in H).
+ Ltac right_ := apply /orP; right.
+ Ltac left_ := apply /orP; left.
+ Ltac split_ := apply /andP; split.   
+
+--------------------- ------------------- ------------------------------*)
+
+
+
+From Coq Require Export ssreflect  ssrbool.
+Set Implicit Arguments.
+
+Section GeneralReflections.
+Definition Prop_bool_eq (P: Prop) (b: bool):= P <-> b=true.
+Check reflect. Print Bool.reflect.
+(* Inductive reflect (P : Prop) : bool -> Set :=  
+   ReflectT : P -> reflect P true | ReflectF : ~ P -> reflect P false *)
+Lemma reflect_elim (P:Prop)(b: bool): reflect P b -> Prop_bool_eq P b.
+Proof. { intro H.
+       split. case H; [ auto | discriminate || contradiction ].
+       case H; [ auto | discriminate || contradiction ]. } Qed. 
+Lemma reflect_intro (P:Prop)(b:bool): Prop_bool_eq P b -> reflect P b.
+Proof. { intros. destruct H as  [H1 H2].
+         destruct b. constructor;auto.
+         constructor.  intro H. apply H1 in H;inversion H. } Qed.
+Hint Immediate  reflect_elim reflect_intro. 
+Lemma reflect_dec P: forall b:bool, reflect P b -> {P} + {~P}.
+Proof. intros b H; destruct b; inversion H; auto.  Qed.
+Lemma reflect_EM P: forall b:bool, reflect P b -> P \/ ~P.
+Proof. intros b H. case H; tauto. Qed.
+Lemma dec_EM P: {P}+{~P} -> P \/ ~P.
+  Proof. intro H; destruct H as [Hl |Hr];tauto. Qed.
+Lemma pbe_EM P: forall b:bool, Prop_bool_eq P b -> P \/ ~P.
+Proof. { intros b H; cut( reflect P b).
+         apply reflect_EM. apply reflect_intro;auto. } Qed.
+Hint Immediate reflect_EM reflect_dec.
+About iffP.
+(* iffP : forall (P Q : Prop) (b : bool), reflect P b -> (P -> Q) -> (Q -> P) -> reflect Q b *)
+About idP.
+(* idP : forall b1 : bool, reflect b1 b1 *)
+Check negP.
+(* negP
+     : reflect (~ ?b1) (~~ ?b1) *)
+Check andP.
+(* andP
+     : reflect (?b1 /\ ?b2) (?b1 && ?b2) *)
+Check orP.
+(*orP
+     : reflect (?b1 \/ ?b2) (?b1 || ?b2) *)
+Lemma impP(p q:bool): reflect (p->q)((negb p) || q).
+  Proof. { case p eqn:pH; case q eqn:qH; simpl.
+       constructor. auto. 
+       constructor. intro H2. cut (false=true). discriminate. apply H2. auto. 
+       constructor. auto. 
+       constructor. discriminate. } Qed. 
+Lemma impP1(P Q:Prop)(p q:bool)(HP: reflect P p)(HQ: reflect Q q):  reflect (P->Q)((negb p) || q).
+Proof. { case p eqn:pH; case q eqn:qH; simpl.
+       constructor. move /HP. apply /HQ.
+       constructor. intro H2. absurd (Q). move /HQ. discriminate. apply H2; apply /HP;auto.
+       constructor. move /HP. discriminate.
+       constructor. move /HP. discriminate. } Qed.
+Lemma switch1(b:bool): b=false -> ~ b.
+Proof. intros H H1.  rewrite H1 in H. discriminate H. Qed.
+Lemma switch2(b:bool): ~ b -> b=false.
+Proof. intros H. case b eqn:H1. absurd (true);auto. auto. Qed.
+
+End GeneralReflections.
+
+
+Hint Immediate reflect_intro reflect_elim  reflect_EM reflect_dec dec_EM: core.
+
+Ltac solve_dec := eapply reflect_dec; eauto.
+Ltac solve_EM  := eapply reflect_EM; eauto.
+
+Ltac switch:=  (apply switch1||apply switch2).
+Ltac switch_in H:= (apply switch1 in H || apply switch2 in H).
+
+Ltac right_ := apply /orP; right.
+Ltac left_ := apply /orP; left.
+Ltac split_ := apply /andP; split.

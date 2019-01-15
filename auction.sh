8102ac9efe57dee514433230fae645c67bd6d301
Abhishek Kr Singh <abhishek.uor@gmail.com>
2019-01-15 22:58:37 +0530

    added compilation for BidAsk Matching and IndRat

--- auction.sh
+++ auction.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
 coqc GenReflect.v
 coqc SetSpecs.v
 coqc Sorting.v
 coqc MinMax.v
+
 coqc DecType.v
 coqc SetReflect.v
 coqc DecList.v
 coqc MoreDecList.v
+
+coqc BidAsk.v
+coqc Matching.v
+coqc IndRat.v
+
+
 

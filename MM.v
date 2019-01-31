10f80e098e171bb9ed489ee4e25462ac388fd09f
Abhishek Kr Singh <abhishek.uor@gmail.com>
2019-01-31 17:10:12 +0530

    first half complete

--- MM.v
+++ MM.v
@@ -21,7 +21,7 @@ Fixpoint produce_MM (B:list Bid) (A: list Ask): (list fill_type) :=
                     |true => ({|bid_of:= b ; ask_of:= a ; tp:=(bp b) |})::(produce_MM B' A')
                     |false => produce_MM B A'
                     end
-  end.
+  end. 
 
 (*------------ Sorting by decreasing ask prices and their properties --------------*)
 
@@ -34,19 +34,28 @@ Proof.  { split.
           { unfold comparable.  unfold by_dsp. intros.
             move /leP in H. apply /leP. omega. } } Qed.
 
-Hint Resolve by_dsp_P: core.
+Lemma by_dsp_refl: reflexive by_dsp.
+Proof. Admitted.
 
 
- Lemma nil_is_MM_forB (B: list Bid): MM_in B nil nil.
+Hint Resolve by_dsp_P by_dsp_refl: core.
+
+
+ Lemma nil_is_MM_forB (B: list Bid): Is_MM nil B nil.
  Proof. Admitted.
  
- Lemma nil_is_MM_forA (A: list Ask): MM_in nil A nil.
+ Lemma nil_is_MM_forA (A: list Ask): Is_MM nil nil A.
  Proof. Admitted.
 
  Hint Resolve nil_is_MM_forB nil_is_MM_forA: core.
+
+ Lemma produce_MM_is_matching (B: list Bid)(A: list Ask):
+   Sorted by_dbp B -> Sorted by_dsp A -> matching_in B A (produce_MM B A).
+ Proof. Admitted.
+ 
            
 Lemma produce_MM_is_MM (B: list Bid)(A: list Ask): Sorted by_dbp B -> Sorted by_dsp A->
-                                                   MM_in B A (produce_MM B A).
+                                                   Is_MM (produce_MM B A) B A.
 Proof. revert B. induction A as [|a A'].
        { (* base case: when A is nil *)
          intros B H H0.  case B. simpl.
@@ -61,18 +70,41 @@ Proof. revert B. induction A as [|a A'].
            { (*------C1:  when b and a are not matchable then produce_MM (b::B') A' *)
              simpl. replace (a <=? b) with false.
              Focus 2. symmetry. apply /leP. omega.
-             assert (h1: MM_in (b::B') A' (produce_MM (b::B') A')).
+             assert (h1: Is_MM  (produce_MM (b::B') A') (b::B') A').
              { apply IHA'. all: eauto. }
-             unfold MM_in. split.
+             unfold Is_MM. split.
              { (*-- matching_in (b :: B') (a :: A') (produce_MM (b :: B') A') ---*)
                destruct h1 as [h1a h1]. eauto. }
              { intros M' h2. destruct h1 as [h1a h1].
                apply h1.
                (* --- Goal: matching_in (b :: B') A' M'---- trivial proof ---*)
-               admit. } }
+               unfold matching_in. split.
+               apply h2. split. apply h2.
+               assert (h3: asks_of M' [<=] (a::A')). apply h2.
+               intros x h4.
+               assert (h5: In x (a::A')). auto. destruct h5.
+               { subst x.
+                 assert (h5: exists m, In m M' /\ a = ask_of m). auto.
+                 destruct h5 as [m h5]. destruct h5 as [h5a h5].
+                 assert (h6: a <= bid_of m).
+                 { subst a. cut (matching M').
+                   unfold matching. intro h6. destruct h6. auto.
+                   apply h2. }
+                 assert (h7:  In (bid_of m) (b::B')).
+                 { cut (In (bid_of m) (bids_of M')).
+                   cut ((bids_of M') [<=] (b::B')). auto.
+                   apply h2. auto. } 
+                 assert (h8: (bid_of m) <= b).
+                 { assert (h9: by_dbp b (bid_of m)). eauto.
+                   unfold by_dbp in h9. apply /leP. auto. }
+                 omega. }
+               { auto. }  } }
            
            { (*-- C2: when b and a are matchable then Output is (b,a):: produce_MM B' A'----*)
-             admit. } } } Admitted.
+             simpl. replace (a <=? b) with true.
+
+
+       } } } Admitted.
                
 
           

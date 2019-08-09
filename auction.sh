f58c0845a87da652b2bb5528461aae049611a28e
Abhishek Kr Singh <abhishek.uor@gmail.com>
2019-08-09 22:50:53 +0530

    compilation progress added

--- auction.sh
+++ auction.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
+
+printf ‘GenReflect
 coqc GenReflect.v
+
+printf ‘SetSpecs.v
 coqc SetSpecs.v
+
+printf ‘Sorting.v
 coqc Sorting.v
+
+printf ‘MinMax.v
 coqc MinMax.v
 
+printf ‘DecType.v
 coqc DecType.v
+
+printf ‘SetReflect.v
 coqc SetReflect.v
+
+printf ‘DecList.v
 coqc DecList.v
+
+printf ‘MoreDecList.v
 coqc MoreDecList.v
 
+printf ‘BidAsk.v
 coqc BidAsk.v
+
+printf ‘Matching.v
 coqc Matching.v
+
+printf ‘AuctionInvar.v
 coqc AuctionInvar.v
+
+printf ‘IndRat.v
 coqc IndRat.v
+
+printf ‘Fair.v
 coqc Fair.v
+
+printf ‘MM.v
 coqc MM.v
+
+printf ‘UM.v
 coqc UM.v
+
+echo \
 
 
 
